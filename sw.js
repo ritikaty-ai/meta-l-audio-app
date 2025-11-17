957d38fe4319f3c94d791818779eca3074b3f94d
ritikaty-ai <ritikaty@gmail.com>
2025-11-17 18:24:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,20 @@
-const CACHE_NAME = 'meta-l-audio-v3';
+const CACHE_NAME = 'meta-l-v1.0';
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
   '/logo.png',
   '/icon-192x192.png',
-  '/icon-512x512.png',
-  '/screenshot1.png',
-  '/screenshot2.png'
+  '/icon-512x512.png'
 ];
 
-// Install & cache everything
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
   );
 });
 
-// Activate & clean old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-// Fetch from cache first, then network
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
