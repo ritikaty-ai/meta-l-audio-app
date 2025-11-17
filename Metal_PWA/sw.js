70cea69816318425c6fd8d4e0ccfad8da205f72c
ritikaty-ai <ritikaty@gmail.com>
2025-11-17 15:45:54 +0530

    Add files via upload
    Initial Meta-L app

---
+++ Metal_PWA/sw.js
@@ -0,0 +1,21 @@
+importScripts("https://progressier.app/5W7klt8g84pkKKxq0AzE/sw.js" );
+const CACHE_NAME = 'metalink-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
