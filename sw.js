45cfa0495efe24475ab1114b02699418935a3d13
ritikaty-ai <ritikaty@gmail.com>
2025-11-17 18:33:10 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'meta-l-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/', '/index.html'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
